fc12983a97cd4eb35527e69662fb2dbcffea2313
Nurlan <nurik.droid@gmail.com>
2019-10-22 01:49:59 +0600

    added get book by id and books by author

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 class LibraryController extends Controller
 {
 
+    public function getBookById(int $id) {
+        $book = Book::where('id', $id)->get();
+        return $book;
+    }
 
+    public function getBooksByAuthor(string $author) {
+        $books = Book::where('author', $author)->get();
+        return $books;
+    }
 
 
 }
