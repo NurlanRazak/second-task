44912ed76014a0b464537b3e028181b219337f71
Nurlan <nurik.droid@gmail.com>
2019-10-22 01:48:52 +0600

    laravel, model book, migration, route

---
+++ database/migrations/2019_10_21_192905_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('author');
+            $table->string('name');
+            $table->integer('year');
+            $table->string('manufacture');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
